23227d365659e50beeed5a8ba32081adf8942c07
Anton Duev <Anton.Duev@broit.io>
2026-06-22 17:58:27 +0300

    Валидация JSON schema. Практикуемся в работе с JSON schema

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,8 +1,12 @@
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestSchema
-from clients.exercises.exercises_client import get_exercises_client, CreateExerciseBodySchema
-from clients.files.files_client import get_files_client, CreateFileRequestSchema
+from clients.courses.courses_client import get_courses_client
+from clients.courses.courses_schema import CreateCourseRequestSchema
+from clients.exercises.exercises_client import get_exercises_client
+from clients.exercises.exercises_schema import CreateExerciseBodySchema
+from clients.files.files_client import get_files_client
+from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
